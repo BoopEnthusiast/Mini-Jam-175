f99febf713ef393c7f6ae846022089f661256378
Trent Shailer <trent.shailer@gmail.com>
2025-01-04 09:38:06 +1300

    Singleplayer automatic spawner

--- objects/spawner.gd
+++ objects/spawner.gd
@@ -1,11 +1,17 @@
 class_name Spawner
 extends Marker2D
 
-
-const SQUARE_BLOCK = preload("res://entities/blocks/SquareBlock.tscn")
+const BLOCKS = [
+	preload("res://entities/blocks/SquareBlock.tscn"),
+	preload("res://entities/blocks/LineBlock.tscn"),
+	preload("res://entities/blocks/TBlock.tscn"),
+	preload("res://entities/blocks/LBlock.tscn"),
+	preload("res://entities/blocks/ZBlock.tscn"),
+]
 
 const SPEED = 100.0
 
+var can_spawn_block := false
 
 func _enter_tree() -> void:
 	MultiplayerSingleton.spawner_node = self
@@ -17,11 +23,40 @@ func _physics_process(delta: float) -> void:
 		return
 	
 	if MultiplayerSingleton.is_singleplayer:
-		pass # TODO: move above player
+		automatic_spawner(delta)
 		
 	else:
 		var direction = Input.get_axis("left", "right")
 		position.x += direction * SPEED * delta
 		
 		if Input.is_action_just_pressed("jump_spawn"):
-			var new_block = SQUARE_BLOCK.instantiate()
+			var new_block = BLOCKS[0].instantiate()
+
+func automatic_spawner(delta: float):
+	if Singleton.player == null:
+		return
+
+	if can_spawn_block:
+		spawn_block()
+
+	position.x = move_toward(position.x, Singleton.player.position.x, SPEED * delta)
+
+func spawn_block():
+	if not can_spawn_block:
+		return
+
+	can_spawn_block = false
+
+	var block_index := randi_range(0, BLOCKS.size() - 1)
+	var new_block = BLOCKS[block_index].instantiate()
+
+	new_block.global_position = global_position
+
+	# Give block a random cardinal rotation
+	var block_rotation := (randi_range(0, 3) / 4.0) * 2.0 * PI
+	new_block.global_rotation = block_rotation
+
+	get_tree().root.add_child(new_block)
+
+func _on_spawn_timer_timeout():
+	can_spawn_block = true
