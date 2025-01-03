1f0c96db1e62dd4f835d47f8f48127ff6983ec5c
Trent Shailer <trent.shailer@gmail.com>
2025-01-03 21:49:34 +1300

    Block can only hit player once

--- entities/blocks/block.gd
+++ entities/blocks/block.gd
@@ -1,16 +1,20 @@
 class_name Block
 extends RigidBody2D
 
+var has_hit_player = false
+
 func _physics_process(_delta):
     if sleeping:
         freeze = true
 
-    for body in get_colliding_bodies():
-        handle_collision(body)
+    if not has_hit_player:
+        for body in get_colliding_bodies():
+            handle_collision(body)
 
 
 func handle_collision(body: Node2D):
     if not (body is Player):
         return
 
+    has_hit_player = true
     print("Block hit player") # TODO replace with damage code
