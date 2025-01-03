9ee8487a38797a60259939dd3d67380a654b7cad
Trent Shailer <trent.shailer@gmail.com>
2025-01-04 10:48:52 +1300

    Impact force based block hit

--- entities/blocks/block.gd
+++ entities/blocks/block.gd
@@ -1,6 +1,11 @@
 class_name Block
 extends RigidBody2D
 
+const HIT_MIN_FORCE: float = 15000.0
+const HIT_MAX_ANGLE: float = 70.0
+const PLAYER_MASS: float = 1.0
+const IMPULSE_DELTA: float = 0.01
+
 var has_hit_player := false
 
 func _physics_process(_delta):
@@ -23,24 +28,38 @@ func handle_collision(contact_index: int, state: PhysicsDirectBodyState2D):
     var body := state.get_contact_collider_object(contact_index)
     if not (body is Player):
         return
-
-    # Hits should have force.
-    if linear_velocity.length() < 50.0:
-        return
-
     var player: Player = body
-    
+
     # Hit must be on the underside of the block, e.g. player jumping on top doesn't count.
     # The contact normal should be within a certain n degrees of Vector2.DOWN.
     var contact_normal := -state.get_contact_local_normal(contact_index)
     var contact_dot_product := Vector2.DOWN.dot(contact_normal)
-    # Contact angle must be <45°
-    if contact_dot_product <= 0.5:
+    if contact_dot_product <= 1.0 - HIT_MAX_ANGLE / 90.0:
         return
+
+    # Hit should occur only if there is a large force applied to the player.
+    # 1 = player, 2 = self
+    # From https://en.wikipedia.org/wiki/Elastic_collision#Two-dimensional
+    var eq_pt_1 := (2 * mass) / (PLAYER_MASS + mass)
+    var eq_pt_2_top := (player.velocity - linear_velocity).dot(player.global_position - global_position)
+    var eq_pt_2_bottom := (player.global_position - global_position).length_squared()
+    var eq_pt_3 := player.global_position - global_position
+    var player_velocity_result := player.velocity - eq_pt_1 * (eq_pt_2_top / eq_pt_2_bottom) * eq_pt_3
+
+    # From https://en.wikipedia.org/wiki/Impulse_(physics)
+    var player_momentum := player.velocity * PLAYER_MASS
+    var player_momentum_result := player_velocity_result * PLAYER_MASS
+    var player_force := (player_momentum_result - player_momentum) / IMPULSE_DELTA
+
+    if player_force.length() < HIT_MIN_FORCE:
+        return
+
+    print(player_force.length())
+
     
     # Hit should be a crush, so the player must be grounded.
-    if not player.is_on_floor():
-        return
+    # if not player.is_on_floor():
+    #     return
 
     has_hit_player = true
     player.health -= 1
