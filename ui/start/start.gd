fde50d3c5d833816cd889a5101716dac463c317a
Trent Shailer <trent.shailer@gmail.com>
2025-01-05 10:16:06 +1300

    Improve Ai's sanity (a little)

--- ui/start/start.gd
+++ ui/start/start.gd
@@ -1,4 +1,13 @@
+class_name Start
 extends Node
+
+@onready var dropper_title := $GridContainer/CenterColumn/MarginContainer/ScrollContainer/VBoxContainer/DropperTitle
+@onready var dropper_controlls := $GridContainer/CenterColumn/MarginContainer/ScrollContainer/VBoxContainer/Control/DropperControlls
+@onready var dropper_objective := $GridContainer/CenterColumn/MarginContainer/ScrollContainer/VBoxContainer/Objective/DropperObjective
+
+@onready var player_title := $GridContainer/CenterColumn/MarginContainer/ScrollContainer/VBoxContainer/PlayerTitle
+@onready var player_controlls := $GridContainer/CenterColumn/MarginContainer/ScrollContainer/VBoxContainer/Control/PlayerControlls
+@onready var player_objective := $GridContainer/CenterColumn/MarginContainer/ScrollContainer/VBoxContainer/Objective/PlayerObjective
 
 var show_dopper_guide := false:
 	set = set_show_dropper_guide
@@ -6,21 +15,21 @@ var show_dopper_guide := false:
 
 func set_show_dropper_guide(value: bool):
 	if value:
-		$GridContainer/CenterColumn/MarginContainer/ScrollContainer/VBoxContainer/DropperTitle.visible = true
-		$GridContainer/CenterColumn/MarginContainer/ScrollContainer/VBoxContainer/Control/DropperControlls.visible = true
-		$GridContainer/CenterColumn/MarginContainer/ScrollContainer/VBoxContainer/Objective/DropperObjective.visible = true
+		dropper_title.visible = true
+		dropper_controlls.visible = true
+		dropper_objective.visible = true
 
-		$GridContainer/CenterColumn/MarginContainer/ScrollContainer/VBoxContainer/PlayerTitle.visible = false
-		$GridContainer/CenterColumn/MarginContainer/ScrollContainer/VBoxContainer/Control/PlayerControlls.visible = false
-		$GridContainer/CenterColumn/MarginContainer/ScrollContainer/VBoxContainer/Objective/PlayerObjective.visible = false
+		player_title.visible = false
+		player_controlls.visible = false
+		player_objective.visible = false
 	else:
-		$GridContainer/CenterColumn/MarginContainer/ScrollContainer/VBoxContainer/DropperTitle.visible = false
-		$GridContainer/CenterColumn/MarginContainer/ScrollContainer/VBoxContainer/Control/DropperControlls.visible = false
-		$GridContainer/CenterColumn/MarginContainer/ScrollContainer/VBoxContainer/Objective/DropperObjective.visible = false
+		dropper_title.visible = false
+		dropper_controlls.visible = false
+		dropper_objective.visible = false
 
-		$GridContainer/CenterColumn/MarginContainer/ScrollContainer/VBoxContainer/PlayerTitle.visible = true
-		$GridContainer/CenterColumn/MarginContainer/ScrollContainer/VBoxContainer/Control/PlayerControlls.visible = true
-		$GridContainer/CenterColumn/MarginContainer/ScrollContainer/VBoxContainer/Objective/PlayerObjective.visible = true
+		player_title.visible = true
+		player_controlls.visible = true
+		player_objective.visible = true
 	
 	show_dopper_guide = value
 
