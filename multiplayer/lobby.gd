953d0d5ad3adf332f713e05aaafa45fe55ef5cad
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-01-04 21:39:19 +1300

    Online multiplayer working !!!
    You have to use playit.gg tho (or another relay server provider)

--- multiplayer/lobby.gd
+++ multiplayer/lobby.gd
@@ -8,8 +8,7 @@ const MAX_CLIENTS = 2
 
 var multiplayer_peer = ENetMultiplayerPeer.new()
 var port = 6969
-var saved_ip: String = "localhost"
-var external_ip: String = ""
+var ip: String = "localhost"
 
 @onready var host_join_label: Label = $CanvasLayer/HostJoinLabel
 @onready var ip_enter: TextEdit = $CanvasLayer/Lobby/LobbyContainer/VBoxContainer2/IPEnter
@@ -21,18 +20,18 @@ func _ready() -> void:
 
 
 func _on_host_pressed() -> void:
-	multiplayer_peer.create_server(port, MAX_CLIENTS)
+	var err = multiplayer_peer.create_server(port, MAX_CLIENTS)
 	multiplayer.multiplayer_peer = multiplayer_peer
-	print("Hosting now")
+	print("Hosting now: " + str(err))
 	host_join_label.text = "Host"
 	MultiplayerSingleton.player_1_id = multiplayer.get_unique_id()
 	lobby.visible = false
 
 
 func _on_join_pressed() -> void:
-	multiplayer_peer.create_client(saved_ip, port)
+	var err = multiplayer_peer.create_client(ip, port)
 	multiplayer.multiplayer_peer = multiplayer_peer
-	print("Joining server")
+	print("Joining server: " + str(err))
 	host_join_label.text = "Client"
 	MultiplayerSingleton.player_2_id = multiplayer.get_unique_id()
 	lobby.visible = false
@@ -67,5 +66,9 @@ func _on_singleplayer_pressed() -> void:
 	lobby.visible = false
 
 
-func _on_ip_text_changed() -> void:
-	saved_ip = ip_enter.text.strip_edges()
+func _on_ip_text_changed(new_text: String) -> void:
+	ip = new_text.strip_edges()
+
+
+func _on_port_text_changed(new_text: String) -> void:
+	port = int(new_text)
