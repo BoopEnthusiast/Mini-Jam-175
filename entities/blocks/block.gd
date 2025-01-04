b388f5a5c30b11e876d50afba5072fcb7de3b05b
Trent Shailer <trent.shailer@gmail.com>
2025-01-04 16:01:26 +1300

    Refactor block code

--- entities/blocks/block.gd
+++ entities/blocks/block.gd
@@ -2,9 +2,7 @@ class_name Block
 extends RigidBody2D
 
 const HIT_MIN_FORCE: float = 15000.0
-const HIT_MAX_ANGLE: float = 70.0
-const PLAYER_MASS: float = 1.0
-const IMPULSE_DELTA: float = 0.01
+const HIT_MIN_DOT: float = 0.33
 
 var has_hit_player := false
 
@@ -14,59 +12,82 @@ func _ready() -> void:
 
 
 func _physics_process(_delta):
-	# if sleeping:
-	#     freeze = true
-
 	if not has_hit_player:
 		var state := PhysicsServer2D.body_get_direct_state(get_rid())
 		var contact_count := state.get_contact_count()
-		# var colliding_bodies := get_colliding_bodies()
+
 		for index in contact_count:
-			# var body := colliding_bodies[index]
 			handle_collision(index, state)
 
 
 func handle_collision(contact_index: int, state: PhysicsDirectBodyState2D):
-	if has_hit_player:
+	if not is_valid_hit(contact_index, state):
 		return
 
+	var player: Player = state.get_contact_collider_object(contact_index)
+	
+	has_hit_player = true
+	player.health -= 1
+	
+	# Destroy after short delay for player to see the hit.
+	get_tree().create_timer(0.05).timeout.connect(queue_free)
+
+	# Apply screenshake
+	if Singleton.camera != null:
+		Singleton.camera.add_trauma(2.0)
+
+
+func is_valid_hit(contact_index: int, state: PhysicsDirectBodyState2D) -> bool:
+	# If the block has already hit the player, invalid hit.
+	if has_hit_player:
+		return false
+
+	# If the hit isn't on the player, invalid hit.
 	var body := state.get_contact_collider_object(contact_index)
 	if not (body is Player):
-		return
-	var player: Player = body
-
+		return false
+	
 	# Hit must be on the underside of the block, e.g. player jumping on top doesn't count.
-	# The contact normal should be within a certain n degrees of Vector2.DOWN.
-	var contact_normal := -state.get_contact_local_normal(contact_index)
-	var contact_dot_product := Vector2.DOWN.dot(contact_normal)
-	if contact_dot_product <= 1.0 - HIT_MAX_ANGLE / 90.0:
-		return
+	if not is_collision_on_underside(contact_index, state):
+		return false
+	
+	# Hit must require a large amount of force to be appled to the player
+	var collision_force := player_collision_force(body)
+	if collision_force < HIT_MIN_FORCE:
+		# print("Collision force not large enough: ", collision_force)
+		return false
 
-	# Hit should occur only if there is a large force applied to the player.
-	# 1 = player, 2 = self
+	# Hit should be a crush, so the player must be grounded.
+	#if not player.is_on_floor():
+	#    return
+	
+	return true
+
+
+func player_collision_force(player: Player) -> float:
+	const PLAYER_MASS: float = 1.0
+	const IMPULSE_DELTA: float = 0.01 # every collision should have a static time
+
+	# Calculate the resulting player velocity from the collision
 	# From https://en.wikipedia.org/wiki/Elastic_collision#Two-dimensional
-	var eq_pt_1 := (2 * mass) / (PLAYER_MASS + mass)
-	var eq_pt_2_top := (player.velocity - linear_velocity).dot(player.global_position - global_position)
-	var eq_pt_2_bottom := (player.global_position - global_position).length_squared()
-	var eq_pt_3 := player.global_position - global_position
-	var player_velocity_result := player.velocity - eq_pt_1 * (eq_pt_2_top / eq_pt_2_bottom) * eq_pt_3
+	var eq_mass := (2 * mass) / (PLAYER_MASS + mass)
+	var eq_vel_dot := (player.velocity - linear_velocity).dot(player.global_position - global_position)
+	var eq_pos_len_sq := (player.global_position - global_position).length_squared()
+	var eq_pos := player.global_position - global_position
+	var player_velocity_result := player.velocity - eq_mass * (eq_vel_dot / eq_pos_len_sq) * eq_pos
 
+	# Calulcate the impulse from the change in velocity and the force required to cause that impulse
 	# From https://en.wikipedia.org/wiki/Impulse_(physics)
 	var player_momentum := player.velocity * PLAYER_MASS
 	var player_momentum_result := player_velocity_result * PLAYER_MASS
 	var player_force := (player_momentum_result - player_momentum) / IMPULSE_DELTA
-
-	if player_force.length() < HIT_MIN_FORCE:
-		return
 	
-	# Hit should be a crush, so the player must be grounded.
-	# if not player.is_on_floor():
-	#     return
+	return player_force.length()
 
-	has_hit_player = true
-	player.health -= 1
-	print("Block hit player")
-	# Destroy after short delay for player to see the hit.
-	get_tree().create_timer(0.05).timeout.connect(queue_free)
 
-	
+func is_collision_on_underside(contact_index: int, state: PhysicsDirectBodyState2D) -> bool:
+	# The contact normal should be within a certain n degrees of Vector2.DOWN.
+	var contact_normal := -state.get_contact_local_normal(contact_index)
+	var contact_dot_product := Vector2.DOWN.dot(contact_normal)
+
+	return contact_dot_product >= HIT_MIN_DOT
